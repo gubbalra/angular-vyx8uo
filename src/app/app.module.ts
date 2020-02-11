846acbe5aa0b3e73c38fcb1df317d210e63642e7
gubbalra <ramesh.gubbala9@gmail.com>
2020-02-11 11:46:19 +0400

    3 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-list/recipe-detail/recipe-detail.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, 
+                  HeaderComponent, 
+                  RecipeListComponent, 
+                  RecipeDetailComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
